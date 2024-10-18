df793b2a32a4e5da98570937b908a17cc17229f9
cvassalos <vassalce@gmail.com>
2024-10-18 18:08:31 -0400

    Finished learning about maps and figuring out when to use what data structure

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -66,7 +66,23 @@ const question = new Map([
 ]);
 console.log(question);
 
+// Convert object to map
 console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours))
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
 
 /*
 //////////////////////////////////////////////////////////////////////////////
