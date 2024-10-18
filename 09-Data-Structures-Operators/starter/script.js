8f1348a4fb28ab2ac6e2054795b8061015eb42e5
cvassalos <vassalce@gmail.com>
2024-10-17 20:12:05 -0400

    Learned map fundamentals and about sets

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,28 +55,108 @@ const restaurant = {
   },
 };
 
-// Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try Again!'],
+]);
+console.log(question);
 
-let openStr = `We are open on ${properties.length} days: `
-for (const day of properties) {
-  openStr += `${day}, `;
-};
-console.log(openStr);
+console.log(Object.entries(openingHours));
 
-// Property VALUES
-const values = Object.values(openingHours)
-console.log(values);
+/*
+//////////////////////////////////////////////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Fireze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-// Entire object
-const entries = Object.entries(openingHours);
-// console.log(entries);
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, 'We are closed :(');
 
-for(const [key, {open, close}] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}`);
-};
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+
+/*
+/////////////////////////////////////////////////////////////////////
+// SETS
+const orderSet = new Set(['Pasta', 
+  'Pizza', 
+  'Pizza', 
+  'Risotto', 
+  'Pasta', 
+  'Pizza'
+]);
+console.log(orderSet);
+
+console.log(new Set('Jonas'));
+
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+orderSet.delete('Risotto');
+// orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('jonasschmedtmann').size);
+*/
+
+// // Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days: `
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// };
+// console.log(openStr);
+
+// // Property VALUES
+// const values = Object.values(openingHours)
+// console.log(values);
+
+// // Entire object
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
+
+// for(const [key, {open, close}] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// };
 
 // ------------------OPTIONAL CHAINING----------------------------------
 // if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
