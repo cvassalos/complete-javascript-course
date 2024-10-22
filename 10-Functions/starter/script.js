2538ce16857800d765eb4b7a803853b487745245
cvassalos <vassalce@gmail.com>
2024-10-22 16:18:32 -0400

    Learned about functions as arguments and functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,7 +25,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 const flight = 'LH234';
 const jonas = {
@@ -44,10 +44,78 @@ const checkIn = function (flightNum, passenger) {
   }
 };
 
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
-const newPassport = funtion(person) {
-    person.passport = Math.trunc(random()) * 100000000000;
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 100000000000);
 };
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+const oneWord = function(str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function(str, fn) {
+  console.log(`Transformed string: ${fn(str)}`)
+
+  console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord); 
+transformer('JavaScript is the best!', oneWord); 
+
+// JS uses callbacks all the time
+const high5 = function() {
+  console.log('👋')
+}
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+const addition = function(a, b) {
+  return ['+', a + b];
+}
+const subtraction = function(a, b) {
+  return ['-', a - b];
+}
+const multiplication = function(a, b) {
+  return ['*', a * b];
+}
+const division = function(a, b) {
+  return ['/', a / b];
+}
+
+const calculate = function(num1, num2, calculation) {
+  const calculated = calculation(num1, num2);
+  console.log(`${num1} ${calculated[0]} ${num2} = ${calculated[1]}`)
+}
+
+calculate(2, 5, addition);
+calculate(2, 5, subtraction);
+calculate(2, 5, multiplication);
+calculate(2, 5, division);
+*/
+
+const greet = function(greeting) {
+  return function(name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Constantinos');
