1316a1d9136f1de4a25b35155500e7838f80fccb
cvassalos <vassalce@gmail.com>
2024-10-29 13:18:52 -0400

    for of vs foreach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -106,7 +106,6 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join('-'));
-*/
 
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -119,3 +118,28 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('--------------- FOR EACH --------------');
+movements.forEach(function (mov, index, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${index + 1} You deposited ${mov}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
