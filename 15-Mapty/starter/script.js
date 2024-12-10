f2d5b7a7358cac44976fbc75cbfb7a76b9003072
cvassalos <vassalce@gmail.com>
2024-12-10 15:53:31 -0500

    Fixed a couple bugs

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -116,24 +116,16 @@ class App {
         const type = inputType.value;
         const distance = +inputDistance.value;
         const duration = +inputDuration.value;
-        console.log(type);
-        console.log(distance);
-
-        // Check if data is valid
-        if(
-            // !Number.isFinite(distance) ||
-            // !Number.isFinite(duration) ||
-            // !Number.isFinite(cadence)
-            !validInputs(distance, duration, cadence) ||
-            !allPositive(distance, duration, cadence)
-        )
-            return alert('Inputs have to be positive numbers!');
 
         // If workout running, create running object
         if(type === 'running') {
             const cadence = +inputCadence.value;
+            // Check if data is valid
             if(
-                !validInputs(distance, duration, elevation) ||
+                // !Number.isFinite(distance) ||
+                // !Number.isFinite(duration) ||
+                // !Number.isFinite(cadence)
+                !validInputs(distance, duration, cadence) ||
                 !allPositive(distance, duration)
             )
                 return alert('Inputs have to be positive numbers!');
@@ -142,6 +134,12 @@ class App {
         // If workout cycling, create cycling object
         if(type === 'cycling') {
             const elevation = +inputElevation.value;
+            // Check if data is valid
+            if(
+                !validInputs(distance, duration, elevation) ||
+                !allPositive(distance, duration);
+            )
+                return alert('Inputs have to be positive numbers!');
         }
 
         // Add new object to workout array
