4b394c47b33859669e06b6361af1bd7340fc9d94
cvassalos <vassalce@gmail.com>
2025-01-07 19:10:54 -0500

    Working with fetch() and promises for APIs

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,12 +4,13 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+// Our first AJAX Call: XMLHttpRequest
+//
 /*
 const getCountryData = function(country) {
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.com/v2/name/${country}`);
-request.send();
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
 request.addEventListener('load', function() {
   const [data] = JSON.parse(this.responseText);
@@ -37,6 +38,7 @@ getCountryData("greece");
 getCountryData("japan");
 */
 
+/*
 const renderCountry = function(data, className = '') {
   const html = `
   <article class="country ${className}">
@@ -88,3 +90,48 @@ const getCountryAndNeighbor = function(country) {
 }
 
 getCountryAndNeighbor("usa");
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+const request = fetch('https://restcountries.com/v2/name/portugal');
+console.log(request);
+*/
+
+const renderCountry = function(data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+/*
+const getCountryData = function(country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      renderCountry(data[0]);
+  });
+};
+*/
+const getCountryData = function(country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal')
