0f35ebbf161faac3b5a90b425768444e5fd14f5e
cvassalos <vassalce@gmail.com>
2025-01-09 12:27:14 -0500

    Working with chaining promises and catching errors

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,28 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function(data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+}
+
+const renderError = function(msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+}
+
 ///////////////////////////////////////
 // Our first AJAX Call: XMLHttpRequest
 //
@@ -99,22 +121,6 @@ const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
 */
 
-const renderCountry = function(data, className = '') {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-}
 /*
 const getCountryData = function(country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
@@ -128,10 +134,32 @@ const getCountryData = function(country) {
   });
 };
 */
+
+
 const getCountryData = function(country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders?.[0];
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+    })
+      .then(response => response.json())
+      .then(data => renderCountry(data, 'neighbour'))
+      .catch(err => {
+	console.error(`${err} ERROR`);
+	renderError(`Something went wrong: ${err.messge}. Try again!`);
+      })
+      .finally(() => {
+	countriesContainer.style.opacity = 1;
+      })
+
 };
 
-getCountryData('portugal')
+btn.addEventListener('click', function() {
+  getCountryData('portugal');
+});
+
