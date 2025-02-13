25faa3a259091f65a4a021b327503ab3f960be62
cvassalos <vassalce@gmail.com>
2025-02-13 13:50:03 -0500

    Promisfying!

---
+++ 16-Asynchronous/starter/build_promise.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const lotteryPromise = new Promise(function(resolve, reject) {
+
+  console.log('Lottery draw is happening');
+  setTimeout(function() {
+    if(Math.random() >= 0.5) {
+      resolve('You WIN!');
+    } else {
+      reject(new Error('You lost your money!'));
+    }
+  }, 2000)
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisfying setTimeout
+const wait = function(seconds) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
