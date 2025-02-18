0944ff4123ae44150cc6e2cbe717048b8d9fedb9
cvassalos <vassalce@gmail.com>
2025-02-18 17:39:25 -0500

    Asycn awaiting stuff

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -237,33 +237,43 @@ try {
   const { latitude: lat, longitude: lng } = pos.coords;
 
   // Reverse geocoding
-  const resGeo = await fetch(`https://api.bigatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+  const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
   if(!resGeo.ok) throw new Error('Problem getting location data')
 
   const dataGeo = await resGeo.json();
-  console.log(dataGeo);
 
   // Country data
   const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.countryCode}`);
   if(!res.ok) throw new Error('Problem getting country data!')
 
   const data = await res.json();
-  console.log(data);
   renderCountry(data[0]); 
+
+  return `You are in ${dataGeo.city}, ${dataGeo.country}`;
 } catch(err) {
-    console.error(`${err}!!!);
+    console.error(`${err}!!!`);
     renderError(`${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 }
-whereAmI();
-console.log('FIRST');
-const codes = fetch('https://restcountries.com/v3.1/all');
-console.log(codes);
 
-// try {
-//   let y = 1;
-// const x = 2;
-//   x = 3;
-// } catch(err) {
-//   alert(err.message);
-// }
+console.log('1: Will get location');
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then(city => console.log(`2. ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💥`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function() {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch(err) {
+    console.error(`2: ${err.message} 💥`)
+  }
+  console.log('3: Finished getting location')
+})();
