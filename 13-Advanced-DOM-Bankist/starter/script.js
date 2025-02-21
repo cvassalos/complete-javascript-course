68aec18fca667eeffce11d50f3cf4179c7f15cbe
cvassalos <vassalce@gmail.com>
2025-02-21 10:17:15 -0500

    Reviewing DOM manipulation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,50 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+ document.getElementById('section--1');
+ const allButtons = document.getElementsByTagName('button');
+ console.log(allButtons);
+
+ console.log(document.getElementsByClassName('btn'));
+
+ // Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got It!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete Elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove();
+});
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
