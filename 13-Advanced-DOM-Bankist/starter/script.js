21a8f11ef6f7669d016ac5799c135dabfec7d33b
cvassalos <vassalce@gmail.com>
2025-03-06 12:36:53 -0500

    Added slider buttons functionality

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -183,7 +183,7 @@ const sectionObserver = new IntersectionObserver
 
 allSections.forEach(function(section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 })
 
 // Lazy loading images
@@ -214,6 +214,47 @@ const imgObserver = new IntersectionObserver(loadImg,
   });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length - 1;
+
+const slider = document.querySelector('.slider')
+slider.style.transform = 'scale(0.4) translateX(-300px)';
+slider.style.overflow = 'visible';
+
+const goToSlide = function(slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
+}
+goToSlide(0);
+
+// Next slide
+const nextSlide = function() {
+  if(curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+
+    goToSlide(curSlide);
+  }
+}
+
+// Prev slide
+const prevSlide = function() {
+  if(curSlide === 0) {
+    curSlide = maxSlide;
+  }
+  curSlide--;
+  goToSlide(curSlide);
+}
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click',  prevSlide);
 
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
